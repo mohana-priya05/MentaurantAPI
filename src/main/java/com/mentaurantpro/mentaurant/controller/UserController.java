f24b1f5ecc3c1a90518bdd03338fc2e17240d6dc
Mohana Priya <mohanabharani480@gmail.com>
2025-01-23 14:24:53 +0530

    commit msg

--- src/main/java/com/mentaurantpro/mentaurant/controller/UserController.java
+++ src/main/java/com/mentaurantpro/mentaurant/controller/UserController.java
@@ -1,4 +1,19 @@
 package com.mentaurantpro.mentaurant.controller;
 
+import com.mentaurantpro.mentaurant.dto.LoginRequestDto;
+import jakarta.persistence.Entity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class UserController {
+    @PostMapping("/login")
+    public String postMethod( LoginRequestDto login) {
+        return "";
+    }
+
+
 }
---
+++ src/main/java/com/mentaurantpro/mentaurant/dto/LoginRequestDto.java
@@ -0,0 +1,4 @@
+package com.mentaurantpro.mentaurant.dto;
+
+public record LoginRequestDto(String username, String password) {
+}
